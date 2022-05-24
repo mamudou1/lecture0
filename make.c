fdae70e83792a70045af5dbbcc23b32bb4d2904a
mamudou1 <mamudoubojang17@gmail.com>
2022-05-24 13:32:46 +0000

    my new c file

---
+++ make.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int number_one, two,three;
+	scanf("%d%d%d",number_one,two,three);
+	printf("enter any three digit numer of your choice");
+ if(two>=three){
+printf("this is the real deal");
+	
+	else {
+	printf("try again for the second time");
+	}
+
+ }
+	return 0;}
